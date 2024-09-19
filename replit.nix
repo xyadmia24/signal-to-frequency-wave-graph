c3d973d125a5de00661dca290a8c96e888b5409e
xyadmia24 <114704323+xyadmia24@users.noreply.github.com>
2024-09-19 01:54:57 +0000

    About took signal input to give me the frequency graph of my own voice linking it back to eeg usage

---
+++ replit.nix
@@ -0,0 +1,16 @@
+{pkgs}: {
+  deps = [
+    pkgs.xsimd
+    pkgs.libxcrypt
+    pkgs.tk
+    pkgs.tcl
+    pkgs.qhull
+    pkgs.pkg-config
+    pkgs.gtk3
+    pkgs.gobject-introspection
+    pkgs.ghostscript
+    pkgs.freetype
+    pkgs.ffmpeg-full
+    pkgs.cairo
+  ];
+}
